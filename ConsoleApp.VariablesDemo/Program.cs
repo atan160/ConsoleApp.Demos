e50aa5844599f4c1508127860b6b5bc6aab61e49
natalija.trifunovic <natalija.trifunovic@DELLDV>
2024-04-19 15:52:00 +0200

    Add project files.

---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different datatypes
+/*
+    text - string 
+    integers - int 
+    decimal - double, float, decimal
+    logical - bool
+*/
+
+string myName = "Natalija"; // Varijabla je dinamicka vrednost
+Console.WriteLine("My name is Natalija"); // Ovo je staticna vrednost
+
+
+Console.WriteLine("My name is " + myName); // String concatenation
+Console.WriteLine($"My name is {myName}"); // String interpolation
+Console.WriteLine("My name is {0}", myName); // Formatted string
+
+int myAge  = 34;
+int x = 0;
+int y = 3;
+
+Console.WriteLine("I am " + myAge + " years old. ");
+
+bool isRetiered = true;
---
+++ ConsoleAppUserInputDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// Declare variables
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+// Prompt the user for input
+Console.WriteLine("Please enter your name");
+name = Console.ReadLine();
+
+Console.WriteLine("Please enter your age");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Proces the data
+int workingYearsLeft = retirementAge - age;
+
+// Output the result to the user
+Console.WriteLine($"Your name is: {name}");
+Console.WriteLine($"Your age is: {age}");
+Console.WriteLine($"Your working years remaining: {workingYearsLeft}");
